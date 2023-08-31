7aa8473c51e7ea4671202d694fe4d8454387bd49
Ahmad nizam z <122218867+ahmadnizamzr@users.noreply.github.com>
2023-08-31 20:47:27 +0700

    Update kodular_alarm.ino

--- kodular_alarm.ino
+++ kodular_alarm.ino
@@ -3,8 +3,8 @@
 #include <MQTT.h>
 //#include <NusabotSimpleTimer.h>
 
-const char ssid[] = "SAFIRUDIN 2";
-const char pass[] = "12345678";
+const char ssid[] = "..";
+const char pass[] = "...";
 
 WiFiClient net;
 MQTTClient client;
